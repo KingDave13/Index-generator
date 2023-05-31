0595193259f46bce8d41619f8be9d54d901081f3
David Okaliwe <okaliwedavid@gmail.com>
2023-05-31 08:42:43 +0100

    adding project

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// let array = [1,2,3,4,5,6,7];
+// console.log('answer:', without(array, 3));
+
 var button = document.getElementById('enter');
 var input = document.getElementById('userinput');
 var ul = document.getElementById('unlisted');
@@ -29,51 +32,51 @@ button.addEventListener('click', addListAfterClick);
 
 input.addEventListener('keypress', addListAfterKeypress);
 
-function isUserValid(bool) {
-	return bool;
-}
+// function isUserValid(bool) {
+// 	return bool;
+// }
 
-var answer = isUserValid(false)? 'You May Enter':'Access Denied';
+// var answer = isUserValid(false)? 'You May Enter':'Access Denied';
 
-var automatedAnswer = 'Your account # is ' + (isUserValid(true)? '1234':'not available');
-
-
-class Animal {
-	constructor (name, colour, type) {
-		this.name =  name;
-		this.colour = colour;
-		this.type = type;
-	}
-}
-
-class Mamal extends Animal {
-	constructor (name, colour, type) {
-		super (name, colour, type)
-	}
-	sound() {
-		console.log (`Moo I'm ${this.name} and I'm a ${this.colour} ${this.type}`);
-	}
-}
-
-const horse = new Mamal ('Jessica', 'white', 'horse');
+// var automatedAnswer = 'Your account # is ' + (isUserValid(true)? '1234':'not available');
 
 
-let obj = {
-	username0: 'David',
-	username1: 'Grace',
-	username2: 'Daniel'
-};
+// class Animal {
+// 	constructor (name, colour, type) {
+// 		this.name =  name;
+// 		this.colour = colour;
+// 		this.type = type;
+// 	}
+// }
 
-Object.entries(obj).forEach(value => {
-	console.log(value);
-})
+// class Mamal extends Animal {
+// 	constructor (name, colour, type) {
+// 		super (name, colour, type)
+// 	}
+// 	sound() {
+// 		console.log (`Moo I'm ${this.name} and I'm a ${this.colour} ${this.type}`);
+// 	}
+// }
 
-Object.entries(obj).map(value => {
-	return value[1] + value[0].replace('username', '');
-})
+// const horse = new Mamal ('Jessica', 'white', 'horse');
 
-const one = () => {
-	const two = () => {
-		console.log(4);
-	}
-}
+
+// let obj = {
+// 	username0: 'David',
+// 	username1: 'Grace',
+// 	username2: 'Daniel'
+// };
+
+// Object.entries(obj).forEach(value => {
+// 	console.log(value);
+// })
+
+// Object.entries(obj).map(value => {
+// 	return value[1] + value[0].replace('username', '');
+// })
+
+// const one = () => {
+// 	const two = () => {
+// 		console.log(4);
+// 	}
+// }
